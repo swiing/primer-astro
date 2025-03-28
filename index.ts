6f05613ece0f1fc623b99ce5fc629ef31cdea155
swiing <persovince@yahoo.fr>
2025-03-28 18:04:30 +0100

    feat: add first set of components

---
+++ index.ts
@@ -0,0 +1,19 @@
+export { default as Text } from "./src/components/text/index.astro"
+
+export { default as Heading } from "./src/components/heading/index.astro"
+
+export { default as Button } from "./src/components/button/index.astro"
+
+export { default as Timeline } from "./src/components/timeline/index.astro"
+export { default as TimelineItem } from "./src/components/timeline/Item.astro"
+
+export { default as Stack } from "./src/components/stack/index.astro"
+
+export { default as Grid } from "./src/components/grid/index.astro"
+export { default as GridColumn } from "./src/components/grid/Column.astro"
+
+export { default as Hero } from "./src/components/hero/index.astro"
+export { default as HeroDescription } from "./src/components/hero/Description.astro"
+export { default as HeroHeading } from "./src/components/hero/Heading.astro"
+export { default as HeroPrimaryAction } from "./src/components/hero/PrimaryAction.astro"
+export { default as HeroSecondaryAction } from "./src/components/hero/SecondaryAction.astro"
